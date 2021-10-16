8ac9d17a033451ff1be913e9ab4b28872e6dd839
Nazmus Sakib <sakibkhancse09@gmail.com>
2021-10-16 12:50:59 +0600

    make some route

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBIVgAc-aUn6SQuHUvejm5-XQNa9ZEgGTM",
+    authDomain: "hot-onion-27e9d.firebaseapp.com",
+    projectId: "hot-onion-27e9d",
+    storageBucket: "hot-onion-27e9d.appspot.com",
+    messagingSenderId: "938161458819",
+    appId: "1:938161458819:web:77e38d948a524bc10195b6"
+  };
+
+  export default firebaseConfig;
